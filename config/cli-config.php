92dc2b19abc5003f5f83090c0524559028d7d2ab
서지혁 <limeburst@ridi.com>
2017-04-24 15:29:38 +0900

    Strict typing

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
--- src/Entity/UserEntity.php
+++ src/Entity/UserEntity.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 namespace Todo\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
